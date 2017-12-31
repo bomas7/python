ee7eea6acf857fbf7c62e1656bb2bed9f9239592
= <=>
2017-12-31 14:42:37 -0500

    Added docstrings

--- tictactoe.py
+++ tictactoe.py
@@ -1,8 +1,10 @@
 def show(grid):
+    """Neatly prints out a tictactoe grid."""
     for row in grid:
         print(' '.join(str(i) for i in row))
 
 def get_move(grid):
+    """Gets the move of a player."""
     vals = []
     print()
     for i in ('row', 'column'):
@@ -23,15 +25,17 @@ def get_move(grid):
         print('That space is taken.')
         return get_move(grid)
 
-
 def check_win(grid, num):
+    """Checks if a player has one."""
     for i in range(3):
         if grid[i].count(num) == 3 or [row[i] for row in grid].count(num) == 3:
             return True
     if grid[0][0] == num and grid[1][1] == num and grid[2][2] == num or grid[0][2] == num and grid[1][1] == num and grid[2][0] == num:
         return True
+    return False
 
 def main():
+    """Plays a game of tictactoe."""
     grid = [[0 for j in range(3)] for i in range(3)]
     message = 'Player{}, please enter your name: '
     players = [input(message.format(i + 1)) for i in range(2)]
