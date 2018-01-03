51ab20da5d9d8874c5df6d0562cbbe527ba8f164
= <=>
2018-01-03 17:55:01 -0500

    Finished hangman program

--- hangman.py
+++ hangman.py
@@ -4,7 +4,6 @@ import string
 
 def get_guess(word):
     guess = input('Guess a letter or a word: ')
-    print(guess)
     for i in guess:
         if i not in string.ascii_letters:
             print('Guess a LETTER or a WORD.\n')
@@ -14,7 +13,7 @@ def get_guess(word):
 def check(guess, word):
     if guess.lower() == word:
         return 'win'
-    elif len(guess) == len(word):
+    elif len(guess) > 1:
         print('That is not the word.')
         return False
     elif guess not in word:
@@ -26,29 +25,30 @@ def check(guess, word):
 def fill_word(guess, masked_word, word):
     for i in range(word.count(guess)):
         masked_word = list(masked_word)
-        masked_word[word.find(guess) + i] = guess 
+        masked_word[''.join(word).find(guess) + i] = guess 
         masked_word = ''.join(masked_word)
         word.remove(guess)
+        print(word)
     return masked_word
 
 def main():
-    print('t' in string.ascii_letters)
     lives = 10
     with open('words.txt') as f:
         word = random.choice(f.readlines()).strip()
     masked_word = '_'*len(word)
     while True:
-        print(f"The word is: {' '.join(list(masked_word))}")
-        print('You currently have {} lives\n'.format(lives))
-        guess = get_guess(word)
-        if check(guess, word) == 'win':
+        if '_' not in masked_word:
             print('The word was: {}'.format(word))
             print('You win!')
             return
-        elif not check(guess, word):
+        print(f"The word is: {' '.join(list(masked_word))}")
+        print('You currently have {} lives\n'.format(lives))
+        guess = get_guess(word)
+        checker = check(guess, word)
+        if not checker:
             lives -= 1
-        elif check(guess, word):
-            masked_word = fill_word(guess, masked_word, word)
+        elif checker:
+            masked_word = fill_word(guess, masked_word, list(word))
         if lives == 0:
             print('The word was: {}'.format(word))
             print('You lose.')
