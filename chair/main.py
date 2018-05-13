c5ea4295354884046359c759a66e55c953df8366
bomas7 <32938412+bomas7@users.noreply.github.com>
2018-05-13 19:42:13 -0400

    Chair Project 1.0

---
+++ chair/main.py
@@ -0,0 +1,68 @@
+from PIL import Image, ImageDraw
+import turtle
+import time
+
+
+class Chair:
+
+    def __init__(self, name, black_pxs=None):
+        self.name = name
+        if black_pxs:
+            self.black_pxs = black_pxs
+        else:
+            self.black_pxs = []
+            im = Image.open(self.name, 'r')
+            black = im.convert('RGB')
+            width, height = im.size
+            for x in range(width):
+                for y in range(height):
+                    if black.getpixel((x, y)) != (255, 255, 255):
+                        self.black_pxs.append((x, y))
+            with open('{}.txt'.format(self.name.split('.')[0]), 'w') as f:
+                f.write('{} = {}\n'.format(self.name, self.black_pxs))
+
+    @classmethod
+    def from_str(cls, str):
+        name, black_pxs = str.split(' = ')
+        return cls(name, eval(black_pxs.rstrip()))
+
+    # def brush_size(self, x, y, size):
+    #     points = []
+    #     for i in range(1, size+1):
+    #         points.append((x+i, y))
+    #         points.append((x-i, y))
+    #         points.append((x, y))
+    #         points.append((x+i, y+i))
+    #         points.append((x-i, y+i))
+    #         points.append((x, y+i))
+    #         points.append((x+i, y-i))
+    #         points.append((x-i, y-i))
+    #         points.append((x, y-i))
+    #     return points
+    #
+    def draw(self):
+        screen = turtle.Screen()
+        screen.title(self.name.title())
+        screen.screensize(750, 1000)
+        drawer = turtle.Turtle()
+        drawer.speed(0)
+        for i in self.black_pxs:
+            drawer.penup()
+            drawer.goto((i[0]-350, 500-i[1]))
+            drawer.pendown()
+            drawer.dot(1.5)
+
+
+
+def main():
+    # for creating new chair coordinates
+    # new = Chair('leftside.jpg')
+    with open('leftside.txt') as f:
+        info = f.read()
+    chair = Chair.from_str(info)
+    chair.draw()
+    quit = input('')
+
+
+if __name__ == '__main__':
+    main()
--- turtle/improved.py
+++ turtle/improved.py
@@ -1,13 +1,21 @@
 import turtle
+import time
+
 
 class Turtle(turtle.Turtle):
 
-    def a(self):
-        self.circle(-10, 90)
-        self.forward(20)
-        self.circle(-10, 360)
+    def a(self, size):
+        self.left(90)
+        self.circle(-size/4, 180)
+        self.forward(size*.9)
+        self.sety(self.ycor()+size/10)
+        self.right(90)
+        self.forward(size/4)
+        self.circle(-size/4, 180)
+        self.forward(size/4)
+        
 
 if __name__ == '__main__':
-tester = Turtle()
-tester.a()
-delay = input('')
+    tester = Turtle()
+    tester.a()
+    time.sleep(2)
