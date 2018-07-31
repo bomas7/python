839bd3c00f8378eea7ae9eeda2e206f3f449aa68
bomas7 <32938412+bomas7@users.noreply.github.com>
2018-07-31 13:55:52 -0400

    rewrote hangman

--- games/hangman.py
+++ games/hangman.py
@@ -1,59 +1,65 @@
+#Decided to rewrite hangman to based off pseudocode
+
+'''
+get word, hidden_form, lives
+
+show hidden and lives left
+get guess from user
+check to see if in word
+update hidden and lives
+repeat until word is completely guessed or lives = 0
+
+ask to play again
+'''
+
+
 import random
 import string
 
 
-def get_guess(word):
-    guess = input('Guess a letter or a word: ')
-    for i in guess:
-        if i not in string.ascii_letters:
-            print('Guess a LETTER or a WORD.\n')
-            return get_guess(word)
-    return guess
+def setup():
+    with open('words.txt') as f:
+        words = f.readlines()
+    word = random.choice(words).strip()
+    hidden = '_'*len(word)
+    return word, hidden, 10
 
-def check(guess, word):
-    if guess.lower() == word:
-        return 'win'
-    elif len(guess) > 1:
-        print('That is not the word.')
-        return False
-    elif guess not in word:
-        print('That letter is not in the word.')
-        return False
+def show_status(hidden, lives):
+    print('\nThe word is: {}'.format(' '.join(hidden)))
+    print(f'You currently have {lives} lives left.\n')
+
+def get_guess():
+    guess = input('Guess a letter: ')
+    if len(guess) == 1 and guess in string.ascii_letters:
+        return guess
+    print('\nGuess a LETTER\n')
+    return get_guess()
+
+def update_status(word, guess, hidden, lives):
+    if guess in word:
+        places = [i for i, j in enumerate(word) if j==guess]
+        hidden = list(hidden)
+        for i in places:
+            hidden[i] = guess
     else:
-        return True
-
-def fill_word(guess, masked_word, word):
-    for i in range(word.count(guess)):
-        masked_word = list(masked_word)
-        masked_word[''.join(word).find(guess) + i] = guess 
-        masked_word = ''.join(masked_word)
-        word.remove(guess)
-    return masked_word
+        print("That letter isn't in the word")
+        lives -= 1
+    return ''.join(hidden), lives
 
 def main():
-    lives = 10
-    with open('words.txt') as f:
-        word = random.choice(f.readlines()).strip()
-    masked_word = '_'*len(word)
-    while True:
-        if '_' not in masked_word:
-            print('The word was: {}'.format(word))
-            print('You win!')
-            return
-        print(f"The word is: {' '.join(list(masked_word))}")
-        print('You currently have {} lives\n'.format(lives))
-        guess = get_guess(word)
-        checker = check(guess, word)
-        if not checker:
-            lives -= 1
-        elif checker:
-            masked_word = fill_word(guess, masked_word, list(word))
-        if lives == 0:
-            print('The word was: {}'.format(word))
-            print('You lose.')
-            return
-        print()
-        
+    word, hidden, lives = setup()
+    print(word)
+    while word != hidden and lives:
+        show_status(hidden, lives)
+        guess = get_guess()
+        hidden, lives = update_status(word, guess, hidden, lives)
+    if lives == 0:
+        print('You lose. The word was: {}'.format(word))
+    else:
+        print('You win! The word was: {}'.format(word))
+    again = input('\nWould you like to play again? (y/n)')
+    if again.lower() == 'y':
+        main()
 
 if __name__ == '__main__':
     main()
